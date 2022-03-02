da45b5ebccc0f217eeeb73e36426a3d4c43ab8fc
Florin Lazar <florin.lazar.it2014@gmail.com>
2022-03-02 19:23:03 +0200

    A simple example with interface implementation

---
+++ OopInterfaces/OopInterfaces/IShape.cs
@@ -0,0 +1,9 @@
+﻿namespace OopInterfaces
+{
+    public interface IShape
+    {
+        double Area { get; }
+
+        void Rotate(double angleDegrees);
+    }
+}
---
+++ OopInterfaces/OopInterfaces/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OopInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = AreaCalculator.SumAreas(
+                new Rectangle(10, 20),
+                new SheetOfPaper(SheetOfPaperFormat.A3));
+            Console.WriteLine(sum);
+        }
+    }
+}
